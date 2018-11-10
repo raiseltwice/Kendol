714189a13508a9f782d3c8bfc69e9ab07c503446
RaiseLtwiCe <raiseltwice@gmail.com>
2018-11-10 03:17:35 +0200

    Test

--- src/main/java/com/raiseltwice/kendol/KendolApplication.java
+++ src/main/java/com/raiseltwice/kendol/KendolApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//kek
 public class KendolApplication {
     public static void main(String[] args) {
         SpringApplication.run(KendolApplication.class, args);
